92b9376e2cb6f5747c5c630a67a9374789e6aea3
chriswongatcuhk <wc38.wong@gmail.com>
2019-02-24 13:40:10 +0800

    Set queryParamsHandling to be merge

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -46,8 +46,8 @@ export class ListEmployeesComponent implements OnInit {
 
   onClick(employeeId: number) {
     this._router.navigate(['/employees', employeeId],{
-      queryParams: { 'searchTerm': this.searchTerm, 'testParam': 'testValue' },
-      queryParamsHandling: 'preserve'
+      queryParams: { 'newParam': 'newValue' },
+      queryParamsHandling: 'merge'
     });
   }
 
